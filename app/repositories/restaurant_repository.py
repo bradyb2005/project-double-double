542721221fa29e178e1be7bc96027f09ae863ae6
Lxamwastaken <anicetoliam@Gmail.com>
2026-09-24 15:50:36 -0700

    Fix hardcoded file path and added error handling to restaurant_repository.py

--- app/repositories/restaurant_repository.py
+++ app/repositories/restaurant_repository.py
@@ -1,11 +1,15 @@
 import json
 
 class RestaurantRepository:
-    def __init__(self):
-        self.filePath = "data/restaurants.json"
+    def __init__(self, file_path: str = "data/restaurants.json"):
+        self.filePath = file_path  
 
     def getAllRestaurants(self):
-        file = open(self.filePath, "r")
-        data = json.load(file)
-        file.close()
-        return data
+        try:
+            with open(self.filePath, "r") as file:
+                data = json.load(file)
+            return data
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File not found: {self.filePath}")
+        except json.JSONDecodeError:
+            raise ValueError(f"Error is not valid JSON: {self.filePath}")
